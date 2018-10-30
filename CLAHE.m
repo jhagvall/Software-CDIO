78c871af19440fd19a89360611ec748c770c7f7e
Julia Hagvall <juliahagvall@gmail.com>
2018-10-30 16:42:27 +0100

    Update CLAHE.m

--- CLAHE.m
+++ CLAHE.m
@@ -1,11 +1,6 @@
 % CLAHE
+function CLAHE_img = CLAHE(varargin)
 
+CLAHE_img = adapthisteq(varargin{1:nargin});
 
-[img] = splitFrames('eye6.avi.MOV', 100);
-
-img_grey = rgb2gray(img);
-
-CLAHE_img = adapthisteq(img_grey, 'NumTiles', [8 8], 'clipLimit', 0.03);
-
-figure(1)
-imshow(CLAHE_img)
+end
