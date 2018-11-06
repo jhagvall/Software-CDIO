a34a67ec069a44dfd00a9b1be7f68650f98aff70
Julia Hagvall <juliahagvall@gmail.com>
2018-11-06 09:17:28 +0100

    Changes

--- segmentation.m
+++ segmentation.m
@@ -2,16 +2,17 @@
 
 %Using frame 100
 %Extracting frame 100 to 109
-stillimg0 = splitFrames('sampleimg (Converted).mov',100);
-stillimg1 = splitFrames('sampleimg (Converted).mov',101);
-stillimg2 = splitFrames('sampleimg (Converted).mov',102);
-stillimg3 = splitFrames('sampleimg (Converted).mov',103);
-stillimg4 = splitFrames('sampleimg (Converted).mov',104);
-stillimg5 = splitFrames('sampleimg (Converted).mov',105);
-stillimg6 = splitFrames('sampleimg (Converted).mov',106);
-stillimg7 = splitFrames('sampleimg (Converted).mov',107);
-stillimg8 = splitFrames('sampleimg (Converted).mov',108);
-stillimg9 = splitFrames('sampleimg (Converted).mov',109);
+[movie,frames] = splitFrames('sampleimg (Converted).mov');
+stillimg0 = movie(100).cdata;
+stillimg1 = movie(101).cdata;
+stillimg2 = movie(102).cdata;
+stillimg3 = movie(103).cdata;
+stillimg4 = movie(104).cdata;
+stillimg5 = movie(105).cdata;
+stillimg6 = movie(106).cdata;
+stillimg7 = movie(107).cdata;
+stillimg8 = movie(108).cdata;
+stillimg9 = movie(109).cdata;
 
 %Converting image to gray scale
 grayimg0 = convert2gray(stillimg0);
@@ -25,57 +26,34 @@ grayimg7 = convert2gray(stillimg7);
 grayimg8 = convert2gray(stillimg8);
 grayimg9 = convert2gray(stillimg9);
 
+% Enhance the image
+enhancedimg0 = imageEnhancement(grayimg0);
+enhancedimg1 = imageEnhancement(grayimg1);
+enhancedimg2 = imageEnhancement(grayimg2);
+enhancedimg3 = imageEnhancement(grayimg3);
+enhancedimg4 = imageEnhancement(grayimg4);
+enhancedimg5 = imageEnhancement(grayimg5);
+enhancedimg6 = imageEnhancement(grayimg6);
+enhancedimg7 = imageEnhancement(grayimg7);
+enhancedimg8 = imageEnhancement(grayimg8);
+enhancedimg9 = imageEnhancement(grayimg9);
 
-%Perfomering histogram equalization & bilinear interpolation
-CLAHEimg0 = CLAHE(grayimg0);
-CLAHEimg1 = CLAHE(grayimg1);
-CLAHEimg2 = CLAHE(grayimg2);
-CLAHEimg3 = CLAHE(grayimg3);
-CLAHEimg4 = CLAHE(grayimg4);
-CLAHEimg5 = CLAHE(grayimg5);
-CLAHEimg6 = CLAHE(grayimg6);
-CLAHEimg7 = CLAHE(grayimg7);
-CLAHEimg8 = CLAHE(grayimg8);
-CLAHEimg9 = CLAHE(grayimg9);
-
-
-%Noise removal using a median filter
-medianimg0 = medfilt2(CLAHEimg0);
-medianimg1 = medfilt2(CLAHEimg1);
-medianimg2 = medfilt2(CLAHEimg2);
-medianimg3 = medfilt2(CLAHEimg3);
-medianimg4 = medfilt2(CLAHEimg4);
-medianimg5 = medfilt2(CLAHEimg5);
-medianimg6 = medfilt2(CLAHEimg6);
-medianimg7 = medfilt2(CLAHEimg7);
-medianimg8 = medfilt2(CLAHEimg8);
-medianimg9 = medfilt2(CLAHEimg9);
-
-%Contrast adjustment
-contrastimg0 = imadjust(medianimg0,stretchlim(medianimg0),[]);
-contrastimg1 = imadjust(medianimg1,stretchlim(medianimg1),[]);
-contrastimg2 = imadjust(medianimg2,stretchlim(medianimg2),[]);
-contrastimg3 = imadjust(medianimg3,stretchlim(medianimg3),[]);
-contrastimg4 = imadjust(medianimg4,stretchlim(medianimg4),[]);
-contrastimg5 = imadjust(medianimg5,stretchlim(medianimg5),[]);
-contrastimg6 = imadjust(medianimg6,stretchlim(medianimg6),[]);
-contrastimg7 = imadjust(medianimg7,stretchlim(medianimg7),[]);
-contrastimg8 = imadjust(medianimg8,stretchlim(medianimg8),[]);
-contrastimg9 = imadjust(medianimg9,stretchlim(medianimg9),[]);
 %%
 % Make image binary
 sense = 0.64;
 
-BW0 = makeBinary(contrastimg0, sense);
-BW1 = makeBinary(contrastimg1, sense);
-BW2 = makeBinary(contrastimg2, sense);
-BW3 = makeBinary(contrastimg3, sense);
-BW4 = makeBinary(contrastimg4, sense);
-BW5 = makeBinary(contrastimg5, sense);
-BW6 = makeBinary(contrastimg6, sense);
-BW7 = makeBinary(contrastimg7, sense);
-BW8 = makeBinary(contrastimg8, sense);
-BW9 = makeBinary(contrastimg9, sense);
+BW0 = makeBinary(enhancedimg0, sense);
+BW1 = makeBinary(enhancedimg1, sense);
+BW2 = makeBinary(enhancedimg2, sense);
+BW3 = makeBinary(enhancedimg3, sense);
+BW4 = makeBinary(enhancedimg4, sense);
+BW5 = makeBinary(enhancedimg5, sense);
+BW6 = makeBinary(enhancedimg6, sense);
+BW7 = makeBinary(enhancedimg7, sense);
+BW8 = makeBinary(enhancedimg8, sense);
+BW9 = makeBinary(enhancedimg9, sense);
+
+
 
 % Fill holes
 BWF0 = imfill(BW0, 'holes');
@@ -89,11 +67,18 @@ BWF7 = imfill(BW7, 'holes');
 BWF8 = imfill(BW8, 'holes');
 BWF9 = imfill(BW9, 'holes');
 
+
 %% Remove connected pixels
+
+% Inverse the image so the black becomes white and vice versa
 compimg = imcomplement(BWF0);
+% Remove objects (groups of white pixels) that are less than 10 000 pixels
 BWF0Rc = bwareaopen(compimg, 10000);
 
+% Inverse it again to make it look like the original
 BWF0R = imcomplement(BWF0Rc);
+
+% Show the images next to eachother to see the difference
 figure(2)
 imshowpair(BWF0, BWF0R, 'montage')
 
@@ -101,6 +86,8 @@ imshowpair(BWF0, BWF0R, 'montage')
 % Count in how many images the pixel is black or white
 binaryAve = pixelAverage(BWF0,BWF1,BWF2,BWF3,BWF4,BWF5,BWF6,BWF7,BWF8,BWF9);
 
+% Show images of original, binary image, binary with filled and the with
+% removed connected pixels
 figure(1)
 subplot(2,2,1)
 imshow(grayimg0)
@@ -116,8 +103,13 @@ imshow(BWF0R)
 
 %% Calculate density
 
+<<<<<<< HEAD
  
-density = nnz(imcomplement(BWF0R))/(size(complement,1)*size(complement,2))
+=======
+% Number of non-zero arguments in the image matrix divided by the total
+% amount of pixels (number of pixels widdth* number of pixels height)
+>>>>>>> 2a172fef99b4223b775512e676e7310a6c936d14
+density = nnz(imcomplement(BWF0R))/(size(BWF0R,1)*size(BWF0R,2))
 
 
 
