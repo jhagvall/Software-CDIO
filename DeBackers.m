528ead44b862934ac980b9512bf631564e9b25b9
Frida Lindqvist <lindqvistfridaa@gmail.com>
2018-11-06 08:39:01 +0100

    Update DeBackers.m

--- DeBackers.m
+++ DeBackers.m
@@ -1,4 +1,4 @@
-function density_of_vessels = DeBackers(image, gridsize, nr_crossing_vessels, size_of_pixel)
+function density_of_vessels = DeBackers(image, gridsize, nr_crossing_vessels, width)
 %- Image is the image on where the density of vessels should be calculated
 %- Gridsize is the number n where the grid nxn is used to calculate the
 %density of vessels
@@ -7,6 +7,7 @@ function density_of_vessels = DeBackers(image, gridsize, nr_crossing_vessels, si
 %- sixe_of_pixel is the physical size of one pixel in mm
 
 [thelength thewidth] = size(image);
+size_of_pixel = width/thewidth;
 pixelsize = size_of_pixel; % How large is one pixel (in mm), will depend on camera and resolution
 
 length_of_lines_pix = (gridsize-1)*thelength + (gridsize-1)*thewidth;
