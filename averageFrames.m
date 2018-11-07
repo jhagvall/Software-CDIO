a756c27035e78376cfa396df0bd8c59ec6378673
Julia Hagvall <juliahagvall@gmail.com>
2018-11-07 09:52:29 +0100

    Checkes for the legnth

--- averageFrames.m
+++ averageFrames.m
@@ -2,12 +2,16 @@ function averimg = averageFrames(movie, startframe, nrframes)
 
 % I0 = im2double(rgb2gray(splitFrames(movie,200)));
 sumimg = im2double(rgb2gray(movie(startframe).cdata));
+movie_length = length(movie);
 
 for n = startframe : startframe+nrframes
-img = rgb2gray(movie(n).cdata);
-sumimg = sumimg + im2double(img);
+    if startframe+nrframes > movie_length
+        averimg = []; 
+    else
+        img = rgb2gray(movie(n).cdata);
+        sumimg = sumimg + im2double(img);
+        averimg = sumimg/nrframes;
+    end
 end
-
-averimg = sumimg/nrframes;
 
 end
