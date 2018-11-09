10a25eae03065f96cf08caf41db14c942ac784d8
Frida Lindqvist <lindqvistfridaa@gmail.com>
2018-11-09 09:39:06 +0100

    Updated function

--- segmentImage.m
+++ segmentImage.m
@@ -1,4 +1,4 @@
-function segmentedimg = segmentImage(enhancedimg,sense)
+function segmentedimg = segmentImage(enhancedimg,sense,pixelremoval)
 
 BW0 = makeBinary(enhancedimg, sense);
 BWF0 = imfill(BW0, 'holes');
@@ -6,8 +6,8 @@ BWF0 = imfill(BW0, 'holes');
 % Inverse the image so the black becomes white and vice versa
 compimg = imcomplement(BWF0);
 
-% Remove objects (groups of white pixels) that are less than 10 000 pixels
-BWF0Rc = bwareaopen(compimg, 10000);
+% Remove objects (groups of white pixels) that are less than "pixelremoval" pixels
+BWF0Rc = bwareaopen(compimg, pixelremoval);
 
 % Inverse it again to make it look like the original
 segmentedimg = imcomplement(BWF0Rc);
