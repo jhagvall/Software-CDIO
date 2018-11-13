295320077f9d445e137c125e13363c9c711b6970
Julia Hagvall <juliahagvall@gmail.com>
2018-11-13 10:53:39 +0100

    Update averageFrames.m

--- averageFrames.m
+++ averageFrames.m
@@ -1,14 +1,14 @@
-function averimg = averageFrames(movie, startframe, nrframes)
+function averimg = averageFrames(video, startframe, nrframes)
 
 % I0 = im2double(rgb2gray(splitFrames(movie,200)));
-sumimg = im2double(rgb2gray(movie(startframe).cdata));
-movie_length = length(movie);
+sumimg = im2double(rgb2gray(video(startframe).cdata));
+video_length = length(video);
 
 for n = startframe : startframe+nrframes
-    if startframe+nrframes > movie_length
+    if startframe+nrframes > video_length
         averimg = []; 
     else
-        img = rgb2gray(movie(n).cdata);
+        img = rgb2gray(video(n).cdata);
         sumimg = sumimg + im2double(img);
         averimg = sumimg/nrframes;
     end
