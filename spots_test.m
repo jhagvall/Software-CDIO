ada244eac34a8244cfe344eb91eab6699e9e2236
Julia Hagvall <juliahagvall@gmail.com>
2018-11-19 15:03:09 +0100

    Update spots_test.m

--- spots_test.m
+++ spots_test.m
@@ -6,7 +6,7 @@ spot_gray = rgb2gray(spot_img);
 enhanced_spot = imageEnhancement(spot_gray);
 % enhanced_spot = medfilt2(enhanced_spot);
 
-thresh = multithresh(spt_gray,2);
+thresh = multithresh(spot_gray,2);
 valuesMax = [thresh max(spot_gray(:))];
 [quant_spot, index] = imquantize(spot_gray,thresh, valuesMax);
 quant_med = medfilt2(quant_spot);
