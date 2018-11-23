6a0978dcabea7114671e420fc6a41d1109f566bf
Frida Lindqvist <lindqvistfridaa@gmail.com>
2018-11-23 13:39:32 +0100

    Update pixelAverage2.m

--- pixelAverage2.m
+++ pixelAverage2.m
@@ -1,4 +1,4 @@
-function output_img = pixelAverage2(image_struct,vidHeight,vidWidth)
+function output_img = pixelAverage2(image_struct,vidHeight,vidWidth,limit)
 ave_img = zeros([vidHeight vidWidth]);
 % Calculates the average binary pixel values
 for i = 1:length(image_struct)
@@ -7,7 +7,7 @@ end
 output = ave_img/length(image_struct);
 
 % Make pixels that are black in majority of images 0
-output(output < 0.6) = 0;
+output(output < limit) = 0;
 
 % Make the image logical, all values >0 will be 1
 output_img=logical(output);
