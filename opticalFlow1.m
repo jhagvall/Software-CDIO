fb1392be2ada984680444e6edb1b4246967b2b5b
Frida Lindqvist <lindqvistfridaa@gmail.com>
2018-11-27 11:57:05 +0100

    Update opticalFlow1.m

--- opticalFlow1.m
+++ opticalFlow1.m
@@ -17,33 +17,17 @@ while hasFrame(vidReader)
     hold off
 end
 
-%%
-figure(1)
-imshow(frameRGB)
-hold on
-plot(flow, 'ScaleFactor',70)
+%% Plot optical flow in every frame - VISA MARCUS
 
-%%
+[video, frames] = splitFrames('testing2.MOV');
+%video = load('stab2.mat');
+%video = video.stabilised_video;
 
-%[video, frames] = splitFrames('S2ST3306.MOV');
-
-video = load('stabilised_IMT8.mat');
-video = video.registered_video;
-
-%%
-
-hf = figure;
-set(hf,'position',[480 640 640 480]);
-
-movie(hf,video,1,50)
-
-%%
-
-opticFlow = opticalFlowLK('NoiseThreshold',0.009);
+opticFlow = opticalFlowHS; %('NoiseThreshold',0.009);
 count = 0;
 
 figure(1)
-for n = 1:100
+for n = 1:length(video)
     video_frame = rgb2gray(video(n).cdata);
 
     flow = estimateFlow(opticFlow,video_frame);
@@ -51,8 +35,63 @@ for n = 1:100
     count = count + 1;
     imshow(video_frame)
     hold on
-    plot(flow,'ScaleFactor',70)
+    plot(flow,'ScaleFactor',70) 
+    drawnow
     hold off
 end
 
 
+%% Plot optical flow with stabilised video 
+
+video = load('stab2.mat');
+video = video.stabilised_video;
+
+%%
+
+opticFlow = opticalFlowHS; %('NoiseThreshold',0.009);
+count = 0;
+
+figure(1)
+for n = 11:length(video)
+    video_frame = im2double(rgb2gray(video(n).cdata));
+
+    flow = estimateFlow(opticFlow,video_frame);
+    
+    count = count + 1;
+    imshow(video_frame)
+    hold on
+    plot(flow,'ScaleFactor',70) 
+    drawnow
+    hold off
+end
+
+%% Plot optical flow in every frame
+
+[video, frames] = splitFrames('testing2.MOV');
+
+opticFlow = opticalFlowHS;
+count = 0;
+flow_saved_Vx = zeros(size(video(50).cdata,1),size(video(50).cdata,2));
+flow_saved_Vy = zeros(size(video(50).cdata,1),size(video(50).cdata,2));
+
+figure(1)
+for n = 1:20
+    video_frame = rgb2gray(video(n).cdata);
+
+    flow = estimateFlow(opticFlow,video_frame);
+  
+    for k = 1:size(flow.Vx,1)
+        for j = 1:size(flow.Vx,2)
+            if flow.Vx(k,j) > 0.1 && flow.Vy(k,j) > 0.1
+                flow_saved_Vx(k,j) = flow.Vx(k,j);
+                flow_saved_Vy(k,j) = flow.Vy(k,j);
+            end
+        end
+    end
+    
+    imshow(video_frame)
+    hold on
+    plot(flow,'ScaleFactor',70) 
+    drawnow
+    hold off
+end
