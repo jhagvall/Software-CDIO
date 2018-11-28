5873888fef17950ab0d8dfdc548f803abb1587db
Julia Hagvall <juliahagvall@gmail.com>
2018-11-28 13:11:53 +0100

    Update imageEnhancement.m

--- imageEnhancement.m
+++ imageEnhancement.m
@@ -1,5 +1,22 @@
 function enhanced_image = imageEnhancement(image)
-claheimg = adapthisteq(image);
+cim=double(image);
+
+[r,c]=size(image);
+cim=cim+1;
+
+% natural log
+lim=log(cim);
+fim=fft2(lim);
+
+lowg=0.9; %(lower gamma threshold, must be lowg < 1)
+highg=1.1; %(higher gamma threshold, must be highg > 1)
+
+him=homomorph(fim,lowg,highg);
+ifim=ifft2(him);
+eim=exp(ifim);
+eim = uint8(real(eim));
+
+claheimg = adapthisteq(eim);
 medianimg = medfilt2(claheimg);
 contrastimg = imadjust(medianimg,stretchlim(medianimg),[]);
 enhanced_image = contrastimg;
