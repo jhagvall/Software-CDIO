5ce5041248a1a3fc711c5825a7d490d0ebe30845
Daniel Bons <daniel.bons@gmail.com>
2018-11-30 13:49:43 +0100

    Updated comments in sizeCalculation

--- sizeCalculation.m
+++ sizeCalculation.m
@@ -1,24 +1,13 @@
 function averagediameter = sizeCalculation(image)
 %%%%%%% Calculate the size of the objects
 % 
-% Input is an image. This will be changed to a function later
+% Input is an image. 
+% This function calculates the average diameter of the vessels in µm. 
+% The calculation will only be correct if the field of view is 1 mm^2
 %
 %%%%%%%
 
-% Right now, this is a script and not a function, the code just below will
-% make the user chose a file. Then it will enhance and segment the image.
-% To open a video file
-% [filename, pathname] = uigetfile( ...
-%     {'*.mov;*.MOV;*.avi;*.mpg;*.mpeg','Video Files (*.mov,*.MOV,*.avi*.mpg,*.mpeg)';
-%      '*.*',  'All Files (*.*)'}, ...
-%      'Select a video file');
-%  
-% 
-% [movie,frames] = splitFrames(filename);
-% img = movie(100).cdata;
-% grayimg = convert2gray(img);
-% enhancedimg = imageEnhancement(grayimg);
-% BW = segmentImage(enhancedimg, 0.64, 10000);
+
 
 
 %% Calculate the euclidean distance between a pixel and the nearest nonzero 
@@ -28,22 +17,10 @@ D = bwdist(image);
 % Extract the skeleton of the binary image
 skeleton = bwmorph(imcomplement(image), 'skel', inf);
 
-% Show the skeleton and the binary image
-% figure(1)
-% imshowpair(skeleton, BW, 'montage')
-
 % Remove the spurs from the skeleton (the small branch points) which is not
 % actually part of the diameter of the vessels
 minSpurs = bwmorph(skeleton,'spur',Inf);
 
-% Show the skeleton with removed spurs and the binary image
-% figure(2)
-% imshowpair(minSpurs, BW, 'montage')
-% title('Skeleton (center of the vessel) and original image')
-
-% Show skeleton with min spurs and original skeleton
-% figure(3)
-% imshowpair(minSpurs, skeleton, 'montage')
 
 % Find the pixels of the spur of the skeleton
 [row, col] = find(minSpurs);
@@ -62,12 +39,12 @@ end
 [~,~, diameterValues] = find(diameter);
 pixeldiameter = mean(diameterValues);
 
+% Get the average diameter in pixels. The image should be 1x1 millimieter
+% which means that pixeldiameter/(mean of the height and width) should give
+% the diameter in mm. Multiply by 1000 to get the unit µm
 averagediameter = pixeldiameter/((size(image,1)+size(image,2))/2)*1000;
 
-% figure(4)
-% imshow(BW)
-% txt = ['Average diameter of the vessels: ' num2str(averageDiameter) ' pixels'];
-% text(4, 0.5, txt)
+
 
 end
 
