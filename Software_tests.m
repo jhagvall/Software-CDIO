88ba059a4e599d67a2ca15c5e683e685e7c428a8
Julia Hagvall <juliahagvall@gmail.com>
2018-11-30 13:30:25 +0100

    Update Software_tests.m

--- Software_tests.m
+++ Software_tests.m
@@ -45,3 +45,58 @@ normalize = normalizeApexes(grayscale);
 segmented = segmentSpot(normalize);
 
 figure,imshow(segmented)
+
+%% Test 23 vessels
+close all
+clear all
+[video,frames,vidWidth, vidHeight,framerate] = splitFrames('S2ST3306.MOV');
+frame_100 = video(100).cdata;
+
+remove_channel = removeChannel(frame_100);
+grayscale = rgb2gray(remove_channel);
+normalize = normalizeVessels(grayscale);
+enhanced = imageEnhancement(normalize);
+
+figure,imshowpair(frame_100,enhanced,'montage')
+
+%% Test 23 own images
+close all
+clear all
+img = imread('Fiber_NewCamera_0.png');
+
+remove_channel = removeChannel(img);
+grayscale = rgb2gray(remove_channel);
+normalize = normalizeVessels(grayscale);
+enhanced = imageEnhancement(normalize);
+
+figure,imshowpair(img,enhanced,'montage')
+
+%% Test 31 own images
+close all
+clear all
+img = imread('Fiber_NewCamera_0.png');
+
+figure,imshow(img)
+
+remove_channel = removeChannel(img);
+grayscale = rgb2gray(remove_channel);
+normalize = normalizeVessels(grayscale);
+enhanced = imageEnhancement(normalize);
+segmented = segmentImage(enhanced,0.64,1000);
+
+figure,detectBoundaries(enhanced,segmented)
+
+%% Test 2
+close all
+clear all
+img = imread('test_12_frame200.png');
+
+remove_channel = removeChannel(img);
+grayscale = rgb2gray(remove_channel);
+normalize = normalizeVessels(grayscale);
+enhanced = imageEnhancement(normalize);
+segmented = segmentSpot(enhanced);
+
+figure,imshowpair(segmented,img,'montage')
+
+%%
