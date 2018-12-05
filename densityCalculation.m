88fc673a27db3603fb4d75bb13753d8d346e5b30
Julia Hagvall <juliahagvall@gmail.com>
2018-12-05 15:59:47 +0100

    Update densityCalculation.m

--- densityCalculation.m
+++ densityCalculation.m
@@ -1,10 +1,7 @@
 function density = densityCalculation(segmentedimage)
-%
-%
 % Takes a segmented image as input argument and calculates the amount of 
 % black pixels as a percentage of all pixels. Returns the density of
-% vessels, the percentage of black pixels
-%
-%
+% vessels, the percentage of black pixels.
 
 density = 100*nnz(imcomplement(segmentedimage))/(size(segmentedimage,1)*size(segmentedimage,2));
+end
