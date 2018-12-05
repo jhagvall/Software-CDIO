e23f3e0a928f0690a2409ca4f8e0d2de417d6ed9
Julia Hagvall <juliahagvall@gmail.com>
2018-12-05 16:18:53 +0100

    Update General/makeBinary.m

--- General/makeBinary.m
+++ General/makeBinary.m
@@ -1,3 +1,6 @@
 function output = makeBinary(img, sense)
+%Creates a binary image using a gray scale image with adaptive threshold
+%using the input sensitivity sense. 
 
 output = imbinarize(img, 'adaptive', 'ForegroundPolarity', 'bright', 'Sensitivity', sense);
+end
