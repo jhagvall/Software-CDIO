26c42f0a6fabd2ae1f893b7cffb770228673699c
Johanssonida <44609151+Johanssonida@users.noreply.github.com>
2018-12-05 16:29:45 +0100

    Update PPV.m

--- Vessels/PPV.m
+++ Vessels/PPV.m
@@ -1,5 +1,7 @@
 function PPV = PPV(tot_vessels, no_flow, intermittent_flow)
-
+%Calculate the proportion of perfused vessels. tot_vessels is the total
+%number of vessels, no_flow is vessels with no flow and intermittent_flow
+%is vessels with intermittents flow in the video. 
 PPV = 100 * ((tot_vessels - (no_flow + intermittent_flow)) / tot_vessels);
 
 end
