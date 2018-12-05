3b491782e11684971964c758cb6ca0c3758cc254
Julia Hagvall <juliahagvall@gmail.com>
2018-12-05 16:24:06 +0100

    Update General/splitFrames.m

--- General/splitFrames.m
+++ General/splitFrames.m
@@ -1,6 +1,7 @@
-%Takes a video and a wanted frame as input to return that certain frame of
-%the input video
 function [video,frames,vidWidth, vidHeight, framerate] =  splitFrames(videoname)
+%Takes a video and a wanted frame as input to return that certain frame of
+%the input video, the total number of frames, the width and height of the
+%video and the frame rate.
 vid = VideoReader(videoname);
 
 vidWidth = vid.Width;
