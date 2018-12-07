fc264724d074632ba7efa4bfc0e54a911c30be9a
Frida Lindqvist <lindqvistfridaa@gmail.com>
2018-12-07 08:38:48 +0100

    Update imageStabilisation.m

--- General/imageStabilisation.m
+++ General/imageStabilisation.m
@@ -5,7 +5,6 @@ function registered_frame = imageStabilisation(video,frame1,frame2)
 % Choose the frames to be registered
 moving = rgb2gray(video(frame1).cdata);
 fixed = rgb2gray(video(frame2).cdata);
-count = 0;
 
 [optimizer,metric] = imregconfig('monomodal');
 
@@ -16,7 +15,6 @@ if temp(3,1) < 7 && temp(3,2) < 7 && temp(3,1) > -7 && temp(3,2) > -7
     movingRegistered = imregister(moving, fixed, 'affine', optimizer,metric);
 else
     movingRegistered = moving;
-    count = 1;
 end
 
 registered_frame = movingRegistered;
